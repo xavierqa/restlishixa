ae8362ad6dbb0f89546fbd81c3a16dc34a60faf9
XavierQA <xavierqa@gmail.com>
2013-05-02 16:56:16 -0700

    adding support for jedis

--- server/src/main/java/com/shixa/impl/DataBaseConnectorImpl.java
+++ server/src/main/java/com/shixa/impl/DataBaseConnectorImpl.java
@@ -4,6 +4,10 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 import com.shixa.formats.User;
 
 public class DataBaseConnectorImpl implements DataBaseConnector {
@@ -11,8 +15,16 @@ public class DataBaseConnectorImpl implements DataBaseConnector {
 	private final AtomicLong _currId = new AtomicLong();
 	private final Map<Long, User> _data = new ConcurrentHashMap<Long, User>();
 
+	private String host = "localhost";
+	
+	private String port = "6379";
+	
+	private JedisPool pool; 
+	
+	private Jedis _jedis; 
 	
 	public DataBaseConnectorImpl(){
+		pool = new JedisPool(new JedisPoolConfig(),host);
 		
 	}
 	
